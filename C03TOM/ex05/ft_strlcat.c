54226af4463e4ff583c204a04ca91d024008aa6f
Thomas Pierre Mathieu Julien Di Benedetto <tdi-bene@c1r12s2.42lisboa.com>
2023-03-11 18:38:29 +0000

    few fixes

--- C03TOM/ex05/ft_strlcat.c
+++ C03TOM/ex05/ft_strlcat.c
@@ -6,22 +6,22 @@
 /*   By: tdi-bene <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 18:17:45 by tdi-bene          #+#    #+#             */
-/*   Updated: 2023/03/10 11:00:54 by tdi-bene         ###   ########.fr       */
+/*   Updated: 2023/03/11 17:16:35 by tdi-bene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int	i;
-	int	j;
+	unsigned int	i;
+	unsigned int	j;
 
 	if (size == 0)
-		return (dest);
+		return (0);
 	i = 0;
 	while (dest[i])
 		i++;
 	if (i >= size)
-		return i;
+		return (size);
 	j = 0;
 	while (src[j] && j + i <= size - 1)
 	{
